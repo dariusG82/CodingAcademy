06092e9432392504ccbbcd6097816e261f9961e0
dariusG82 <94805075+dariusG82@users.noreply.github.com>
2022-02-17 19:14:16 +0200

    Update

---
+++ 2022-02-17/src/js/core.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(index){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if(index > slides.length){
+        slideIndex = 1;
+    }
+
+    if(index < 1){
+        slideIndex = slides.length; 
+    } 
+
+    for(i = 0; i< slides.length;i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
